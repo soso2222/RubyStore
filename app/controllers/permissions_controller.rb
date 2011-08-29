2ed8ca5e600ac7334bdc30857daa460592a47529
Danail Dekov <dannydekov@gmail.com>
2011-08-29 22:06:53 +0300

    Permissions

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,3 +1,82 @@
 class PermissionsController < ApplicationController
 
+  
+  def index
+    check_permissions()
+    @roles = Role.all
+    @permissions = Permission.all
+    
+    
+    
+    @custompermissions = Array.new
+    @permissions.each do |permission|
+      @returned_permission = exist(permission, @custompermissions)
+      if @returned_permission == nil
+        @custom_permission = CustomPermissions.new
+        @custom_permission.controller = permission.controller
+        @custom_permission.methods = Array.new
+        @custom_permission.methods << permission.method
+        @custompermissions << @custom_permission
+      else
+        @returned_permission.methods << permission.method
+      end
+    end
+    
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @groups }
+    end
+  end
+  
+  private
+  def exist(permission, list)
+    list.each do |custom_permission|
+      if custom_permission.controller == permission.controller
+        return custom_permission
+      end
+    end  
+    return nil
+  end
+
+  private
+  def check_permissions
+    
+    
+    
+    #ApplicationController.send(:subclasses).each do |controller_name|
+    
+
+    
+    #classname = controller_name
+    
+    classnames = ['GroupsController','RolesController','PermissionsController']
+    classnames.each do |classname|
+    
+    a= Kernel.const_get(classname).instance_methods(false)
+    #a= classname.instance_methods(false)
+  
+    a.each do |method| 
+        
+      p = Permission.new
+      p.controller = classname
+      p.method = method
+      p.role_ids = []
+      
+  
+      s = Permission.all(:conditions=>{:controller=>classname, :method=>p.method})
+      #s = Permission.all
+  
+        if s.empty?
+          p.save
+        end
+      end 
+    #end
+    
+    
+    end
+    
+  end
+
+
 end
---
+++ lib/custom_permissions.rb
@@ -0,0 +1,4 @@
+
+class CustomPermissions
+       attr_accessor :controller, :methods
+end
